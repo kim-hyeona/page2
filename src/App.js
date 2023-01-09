4a5325ec2bb7be7e9ee273b5c4e081088a92c3df
kim-hyeona <kim-hyeona991113@github.com>
2023-01-09 12:16:21 +0900

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Checkbox from "./component/Checkbox";
+
+function App(props) {
+  return (
+    <>
+      {/* <h1>App.js</h1> */}
+      <Checkbox/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Checkbox.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const dataList = [
+    {id:1,name:'chk1',text:'나이확인',isChk:true},
+    {id:2,name:'chk2',text:'약관확인',isChk:false},
+    {id:3,name:'chk3',text:'정보수집확인',isChk:false},
+    {id:4,name:'chk4',text:'혜택확인',isChk:false},
+]
+
+function Checkbox(props) {
+
+    const [data,setData] = useState(dataList);
+
+    const changeInput = (e) => {
+        const {checked,name} = e.target;
+        if(name === 'all'){
+        setData(data.map(item => {
+            return {
+                ...item,
+                isChk:checked,
+            }
+        }));
+    } else {
+        setData(
+            data.map(item => item.name === name ? {...item, isChk : checked }:item)
+        )
+    }
+    }
+    return (
+        <>
+         <input type="checkbox" name='all'onChange={changeInput} checked={data.filter(item => item.isChk !== true).length < 1} />
+         <label>전체동의</label>   
+         <hr />
+
+        {data.map(info => <p key={info.id}>
+            <input type='checkbox' checked={info.isChk} name={info.name} onChange={changeInput}/>
+            <label>{info.text}</label> </p>)}
+
+       {/*   <input type="checkbox" />
+         <label>나이확인</label>
+         <input type="checkbox" />
+         <label>약관확인</label>
+         <input type="checkbox" />
+         <label>정보수집확인</label>
+         <input type="checkbox" />
+         <label>혜택확인</label> */}
+
+        </>
+    );
+}
+
+export default Checkbox;
